12268f7e604e715c5434a5122771496ec8ef37b5
Jordan <personal.jordanabraham@gmail.com>
2021-12-21 16:19:51 -0500

    Format.

--- src/main/kotlin/com/runetopic/plugins/Koin.kt
+++ src/main/kotlin/com/runetopic/plugins/Koin.kt
@@ -1,10 +1,10 @@
 package com.runetopic.plugins
 
-import com.runetopic.mongodb.MongoClient
 import com.runetopic.api.tools.npc.npcModule
 import com.runetopic.api.tools.obj.objModule
 import com.runetopic.api.topics.topicModule
 import com.runetopic.api.user.userModule
+import com.runetopic.mongodb.MongoClient
 import io.ktor.application.*
 import org.koin.dsl.module
 import org.koin.ktor.ext.Koin
