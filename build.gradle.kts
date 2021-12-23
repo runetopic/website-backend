5a5a8b38702b22ff3624dddf18865029272dc570
Jordan <personal.jordanabraham@gmail.com>
2021-12-22 23:29:19 -0500

    Update LogBack and Jackson DataType JSR310 dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val koin_version: String by project
-val mockk_version: String by project
-val kmongo_version: String by project
+val ktorVersion: String by project
+val logbackVersion: String by project
+val koinVersion: String by project
+val mockkVersion: String by project
+val kmongoVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.6.10"
@@ -15,14 +14,6 @@ plugins {
 group = "com.runetopic.website"
 version = "1.0.0-SNAPSHOT"
 
-tasks {
-    shadowJar {
-        manifest {
-            attributes(Pair("Main-Class", "com.runetopic.ApplicationKt"))
-        }
-    }
-}
-
 application {
     mainClass.set("com.runetopic.ApplicationKt")
 }
@@ -31,31 +22,31 @@ repositories(RepositoryHandler::mavenCentral)
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("io.ktor:ktor-auth:$ktor_version")
-    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-jackson:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-jackson:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
-    implementation("org.litote.kmongo:kmongo:$kmongo_version")
-    implementation("org.litote.kmongo:kmongo-async:$kmongo_version")
-    implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
-    implementation("org.litote.kmongo:kmongo-reactor:$kmongo_version")
-    implementation("org.litote.kmongo:kmongo-id:$kmongo_version")
+    implementation("org.litote.kmongo:kmongo:$kmongoVersion")
+    implementation("org.litote.kmongo:kmongo-async:$kmongoVersion")
+    implementation("org.litote.kmongo:kmongo-coroutine:$kmongoVersion")
+    implementation("org.litote.kmongo:kmongo-reactor:$kmongoVersion")
+    implementation("org.litote.kmongo:kmongo-id:$kmongoVersion")
 
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
 
-    implementation("io.insert-koin:koin-core:$koin_version")
-    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
 }
 
 tasks.withType<Test> {
     dependencies {
         testImplementation("org.jetbrains.kotlin:kotlin-test")
-        testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
-        testImplementation("io.mockk:mockk:$mockk_version")
+        testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+        testImplementation("io.mockk:mockk:$mockkVersion")
     }
 }
 
@@ -72,5 +63,10 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
         kotlinOptions.freeCompilerArgs = listOf("-Xopt-in=kotlin.time.ExperimentalTime")
+    }
+    shadowJar {
+        manifest {
+            attributes(Pair("Main-Class", "com.runetopic.ApplicationKt"))
+        }
     }
 }
